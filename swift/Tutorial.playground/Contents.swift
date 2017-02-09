2f636a6996e8508a6034ac702a1643cdb5dc45d2
Jayant Arora <jaistudy1996@gmail.com>
2017-02-09 15:18:40 -0500

    Control flow complete

--- swift/Tutorial.playground/Contents.swift
+++ swift/Tutorial.playground/Contents.swift
@@ -43,4 +43,79 @@ let emptyArray = [String]()
 let emptyDict = [String: Float]()
 // if type info is known use empty [] and [:] for array and dict respectively.
 
+// ####### CONTROL FLOW #######
 
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores{
+    if score > 50{
+        teamScore+=3
+    }
+    else{
+        teamScore+=1
+    }
+}
+print(teamScore)
+
+
+// Optional values: they help with values that are not available.
+// They either have a nil or have a value.
+var optionalName: String? = "Jay"
+print(optionalName == nil)
+
+var optionalName2: String? = "Jay Arora"
+var greeting = "hello"
+
+// WIll only work if optioalName2 is not nill
+if let name = optionalName2{
+    greeting = greeting + name
+}
+else{
+    greeting = "Hello world"
+}
+
+//Another way to use optional value is to use ?? operator
+
+let nickName: String? = "jay"
+let fullName: String = "Jayant Arora"
+let informalGreet = "Hello \(nickName ?? fullName)"
+
+// Switch statements
+// no need to break after case statements
+let vegetable = "red pepper"
+switch vegetable{
+    case "celery":
+        print("YOu are celery ")
+    case "red pepper":
+        print("Great you ate red pepper")
+    default:
+        print("I have no idea what you ate.")
+}
+
+// For-in loop for dictionaries
+let interestingNumbers = ["Prime": [2, 3, 5, 7, 11, 13], "Fibonacci": [1, 1, 2, 3, 5, 8], "Square": [1, 4, 9, 16, 25]]
+
+var largest = 0
+for (keys, values) in interestingNumbers{
+    for number in values{
+        if number > largest{
+            largest = number
+        }
+    }
+}
+print(largest)
+
+// While loop
+var n = 1
+while n<5{
+    n+=1
+}
+print(n)
+
+// Will work first and then check for condition
+var m = 7
+repeat{
+    m += 1
+}while m<7
+
+print(m)
