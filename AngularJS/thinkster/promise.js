aa91212288980d896a0a2d3e82e78b8d4d71cca3
Jayant Arora <jaistudy1996@gmail.com>
2017-02-19 22:58:35 -0500

    Implement promise rejection

--- AngularJS/thinkster/promise.js
+++ AngularJS/thinkster/promise.js
@@ -1,8 +1,25 @@
+// Resolve function. 
+
+// function getData($timeout, $q){
+// 	return function(){
+// 		var defer = $q.defer();
+// 		$timeout(function(){
+// 			defer.resolve("resolved this promise");
+// 		}, 2000);
+// 		return defer.promise;
+// 	}
+// }
+
 function getData($timeout, $q){
 	return function(){
-		var defer = $q.defer();
+		var defer = $q.defer(); // Define a promise
 		$timeout(function(){
-			defer.resolve("resolved this promise");
+			if(Math.round(Math.random())){
+				defer.resolve("Data has been received.");
+			}
+			else{
+				defer.reject("Rejected!");
+			}
 		}, 2000);
 		return defer.promise;
 	}
@@ -14,6 +31,8 @@ app.factory('getData', getData)
 .run(function(getData){
 	var promise = getData();
 	promise.then(function(string){
+		console.log(string);
+	}, function(string){
 		console.log(string);
 	});
 })
